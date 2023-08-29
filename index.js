efda1046ced8082f5df1a6254a1e9eea6433cc25
pavan rayabagi <68803406+pavan731@users.noreply.github.com>
2023-08-29 19:15:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
   res.send('Hello, MongoDB!Great we have done it manually');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 443;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
