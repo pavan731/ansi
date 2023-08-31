520e20cadf04a10f450c3885446958c26fbec5ee
pavan rayabagi <68803406+pavan731@users.noreply.github.com>
2023-08-31 17:03:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
       <style>
         body {
           font-family: Arial, sans-serif;
-          background-color: #f0f0f0;
+          background-color: #000000;
           margin: 0;
           padding: 0;
           display: flex;
